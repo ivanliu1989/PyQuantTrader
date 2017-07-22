04ec4f7738dc242c0fa669a88dfb80e3a4a843d0
Ivan Liu <ivan.liuyanfeng@gmail.com>
2017-07-22 22:51:52 +1000

    structure of package

---
+++ PyQuantTrader/engines/engines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+"""
+Created on Sat Jul 22 22:46:51 2017
+
+@author: Ivan Liu
+"""
+
+class BackTestingEngines():
+    
+    def deployStrategy(self):
+        return None
+    
+    def removeStrategy(self):
+        return None
+    
+    def updateStrategy(self):
+        return None
+    
+    
+
+class LiveABTestingEngines():
+    
+    def deployStrategy(self):
+        return None
+    
+    def removeStrategy(self):
+        return None
+    
+    def updateStrategy(self):
+        return None    
---
+++ PyQuantTrader/performance/performance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+"""
+Created on Sat Jul 22 22:46:51 2017
+
+@author: Ivan Liu
+"""
+
+class performanceAnalytics():
+    
+    def extractPerformance(self):
+        return None
+    
