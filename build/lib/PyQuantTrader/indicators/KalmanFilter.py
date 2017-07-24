f65cc77d7af9c9e5a38ea2aec35e8366ff86c2dd
Ivan Liu <ivan.liuyanfeng@gmail.com>
2017-07-24 23:44:10 +1000

    kalmanfilter indicators

--- build/lib/PyQuantTrader/indicators/KalmanFilter.py
+++ build/lib/PyQuantTrader/indicators/KalmanFilter.py
@@ -57,14 +57,18 @@ class KalmanSignals(bt.Indicator):
         # shortexit is et < sqrt_qt ... the opposite of short
         
         
-class KalmanFilterMA(bt.indicators.MovingAverageBase):
-    alias = ('KalmanMovingAverage',)
+class KalmanMovingAverage(bt.indicators.MovingAverageBase):
+    packages = ('pykalman',)
+    frompackages = (('pykalman', [('KalmanFilter', 'KF')]),)
     lines = ('kma',)
+    alias = ('KMA',)
     params = (
         ('initial_state_covariance', 1.0),
         ('observation_covariance', 1.0),
         ('transition_covariance', 0.05),
     )
+
+    plotlines = dict(cov=dict(_plotskip=True))
 
     def __init__(self):
         self.addminperiod(self.p.period)  # when to deliver values
@@ -88,3 +92,54 @@ class KalmanFilterMA(bt.indicators.MovingAverageBase):
     def next(self):
         k1, self._c1 = self._kf.filter_update(self._k1, self._c1, self.data[0])
         self.lines.kma[0] = self._k1 = k1
+        
+        
+class NumPy(object):
+    packages = (('numpy', 'np'),)
+
+
+class KalmanFilterInd(bt.Indicator, NumPy):
+    _mindatas = 2  # needs at least 2 data feeds
+
+    packages = ('pandas',)
+    lines = ('et', 'sqrt_qt')
+
+    params = dict(
+        delta=1e-4,
+        vt=1e-3,
+    )
+
+    def __init__(self):
+        self.wt = self.p.delta / (1 - self.p.delta) * np.eye(2)
+        self.theta = np.zeros(2)
+        self.P = np.zeros((2, 2))
+        self.R = None
+
+        self.d1_prev = self.data1(-1)  # data1 yesterday's price
+
+    def next(self):
+        F = np.asarray([self.data0[0], 1.0]).reshape((1, 2))
+        y = self.d1_prev[0]
+
+        if self.R is not None:  # self.R starts as None, self.C set below
+            self.R = self.C + self.wt
+        else:
+            self.R = np.zeros((2, 2))
+
+        yhat = F.dot(self.theta)
+        et = y - yhat
+
+        # Q_t is the variance of the prediction of observations and hence
+        # \sqrt{Q_t} is the standard deviation of the predictions
+        Qt = F.dot(self.R).dot(F.T) + self.p.vt
+        sqrt_Qt = np.sqrt(Qt)
+
+        # The posterior value of the states \theta_t is distributed as a
+        # multivariate Gaussian with mean m_t and variance-covariance C_t
+        At = self.R.dot(F.T) / Qt
+        self.theta = self.theta + At.flatten() * et
+        self.C = self.R - At * F.dot(self.R)
+
+        # Fill the lines
+        self.lines.et[0] = et
+        self.lines.sqrt_qt[0] = sqrt_Qt
