e3af19f920ab16e701177366b09adfe910546470
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2017-08-23 22:36:46 +1000

    oanda v20

---
+++ strategies/oandaV20.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 22:30:31 2017
+
+@author: sky_x
+"""
+
+exactbars = 1
+stopafter = 0
+token = "8153764443276ed6230c2d8a95dac609-e9e68019e7c1c51e6f99a755007914f7"
+account = "101-011-6029361-001"
+account_type = "practice"
+live = False
+qcheck=0.5
+data0= 'AUD_USD'
+data1= 'USD_CAD'
+bidask= True
+historical = True
+fromdate = '2016-12-01T12:00:00'
+smaperiod = 5
